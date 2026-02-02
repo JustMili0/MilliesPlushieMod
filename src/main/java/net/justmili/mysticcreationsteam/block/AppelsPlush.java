e59d487ffe50ef6825a2d355b3ffc9c1486cf2be
JustMili <rodx160@gmail.com>
2026-02-02 19:51:20 +0100

    Lil fix of assigned block model textures

--- src/main/java/net/justmili/mysticcreationsteam/block/AppelsPlush.java
+++ src/main/java/net/justmili/mysticcreationsteam/block/AppelsPlush.java
@@ -17,7 +17,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class AppelsPlush extends Block {
     public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
-    private static final VoxelShape BOUNDING_BOX = Block.box(2, 0, 2, 14, 16, 14);
+    private static final VoxelShape BOUNDING_BOX = Block.box(2, 0, 2, 14, 14, 14);
 
     public AppelsPlush() {
         super(Properties.of()
