f07136ea9e44527237e0b796cde0a82d6a640c8e
JustMili <rodx160@gmail.com>
2026-02-10 20:11:50 +0100

    Updated client rendering

--- src/main/java/net/justmili/mysticcreationsteam/client/MCTPlushiesClient.java
+++ src/main/java/net/justmili/mysticcreationsteam/client/MCTPlushiesClient.java
@@ -2,27 +2,19 @@ package net.justmili.mysticcreationsteam.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.ChunkSectionLayerMap;
-import net.justmili.mysticcreationsteam.init.PlushBlocks;
+import net.justmili.mysticcreationsteam.init.PlushResourceLists;
 import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+import net.minecraft.world.level.block.Block;
 
 public class MCTPlushiesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ChunkSectionLayerMap.putBlock(PlushBlocks.BASETEST_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.ENO_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.MILLIE_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.APPELS_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.MAHAN_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.MOCHILA_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.GAEL_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.DGIRAFFE_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.FAYE_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.JOHN_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.PLUME_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.RAZE_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.MARY_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.IZYA_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.BLU_PLUSH, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(PlushBlocks.NEMESTRO_PLUSH, ChunkSectionLayer.CUTOUT);
+        for (Block plush : PlushResourceLists.getPlushies()) {
+            setCutout(plush);
+        }
+    }
+
+    private static void setCutout(Block block) {
+        ChunkSectionLayerMap.putBlock(block, ChunkSectionLayer.CUTOUT);
     }
 }
